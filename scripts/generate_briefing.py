aa4c75034761fa63fe02703ad67b34e2a282c233
missophs <melissaw212@gmail.com>
2026-07-23 12:34:50 -0400

    Add 16 Handles to auto-trash list

--- scripts/generate_briefing.py
+++ scripts/generate_briefing.py
@@ -59,6 +59,7 @@ NEWSLETTER_TRASH_PATTERNS = [
     "quillbot",
     "phil strazzulla",
     "limitless creator",
+    "16handles",
 ]
 
 # Emails from these senders are NEVER auto-trashed and are force-rescued if in trash.
